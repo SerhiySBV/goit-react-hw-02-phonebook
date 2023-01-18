3dc1f70c126cc13eb4a84ee38b90c1304b50f2e4
SerhiySBV <serhiybelyavskii@gmail.com>
2023-01-18 23:16:27 +0200

    added contacts to state by submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from 'components/ContactForm/ContactForm';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  addSubmitUserData = contact => {
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addSubmitUserData} />
+
+        <h2>Contacts</h2>
+        {/* <Filter ... /> */}
+        {/* <ContactList ... /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
